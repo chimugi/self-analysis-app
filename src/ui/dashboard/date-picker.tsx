f580d57eb1e83d50751ad539546a79fa6fd1c854
chimugi <ttt.04139200@gmail.com>
2024-10-13 12:19:34 +0900

    modify date-picker

--- src/ui/dashboard/date-picker.tsx
+++ src/ui/dashboard/date-picker.tsx
@@ -2,7 +2,16 @@
 import { convertToPascalCaseWithSpaces } from "@/lib/util";
 import { useState } from "react";
 import DatePicker from "react-datepicker";
+import { getMonth, getYear } from 'date-fns';
 import "react-datepicker/dist/react-datepicker.css";
+
+function range(start: number, end: number, step: number = 1): number[] {
+  const result = [];
+  for (let i = start; i < end; i += step) {
+    result.push(i);
+  }
+  return result;
+}
 
 export default function MyDatePicker(
   { id, defaultValue }: { id: 'eventDate' | 'startDate' | 'endDate', defaultValue?: Date}
@@ -10,10 +19,65 @@ export default function MyDatePicker(
   const [date, setDate] = useState<Date | null>(defaultValue ?? null);
   const convertedId = convertToPascalCaseWithSpaces(id);
 
+  const years = range(1990, getYear(new Date()) + 1, 1);
+  const months = [
+    "January", "February", "March", "April",
+    "May", "June", "July", "August",
+    "September","October", "November", "December",
+  ];
+
   return (
     <div>
       <div className="mt-2">{convertedId}</div>
       <DatePicker
+        renderCustomHeader={({
+          date,
+          changeYear,
+          changeMonth,
+          decreaseMonth,
+          increaseMonth,
+          prevMonthButtonDisabled,
+          nextMonthButtonDisabled,
+        }) => (
+          <div
+            style={{
+              margin: 10,
+              display: "flex",
+              justifyContent: "center",
+            }}
+          >
+            <button onClick={decreaseMonth} disabled={prevMonthButtonDisabled}>
+              {"<"}
+            </button>
+            <select
+              value={getYear(date)}
+              onChange={({ target: { value } }) => changeYear(value as unknown as number)} // TODO: Fix assertion
+            >
+              {years.map((option) => (
+                <option key={option} value={option}>
+                  {option}
+                </option>
+              ))}
+            </select>
+
+            <select
+              value={months[getMonth(date)]}
+              onChange={({ target: { value } }) =>
+                changeMonth(months.indexOf(value))
+              }
+            >
+              {months.map((option) => (
+                <option key={option} value={option}>
+                  {option}
+                </option>
+              ))}
+            </select>
+
+            <button onClick={increaseMonth} disabled={nextMonthButtonDisabled}>
+              {">"}
+            </button>
+          </div>
+        )}
         id={id}
         name={id}
         selected={date}
