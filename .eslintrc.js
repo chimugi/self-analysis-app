b6acc6b19b82b318f1561f14c80f9e205b57a7d4
chimugi <ttt.04139200@gmail.com>
2024-10-16 23:26:50 +0900

    setting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,11 @@
 module.exports = {
   root: true,
-  extends: ['plugin:@typescript-eslint/recommended', 'next/core-web-vitals', 'prettier'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'next/core-web-vitals',
+    'plugin:tailwindcss/recommended',
+    'prettier'
+  ],
   plugins: ['unused-imports'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
